2087dd6e2477eee441544d62821a09464d4321bd
nyabangWech <nyabangwech@gmail.com>
2024-03-08 19:48:44 +0300

    added files

---
+++ index.js
@@ -0,0 +1,36 @@
+
+// Extract the last four characters from the string below;"extravaganza"
+let e="extravaganza"
+console.log({characters:a[8]});
+console.log({characters:n[9]});
+console.log({characters:z[10]});
+console.log({characters:a[11]});
+
+
+
+const  foods="the quick brown fox jumps over the lazy dog";
+const  addString =foods.slices(0,4) + "eat" + foods.slice(4);
+console.log({addString});
+
+
+const story="The quick brown foxs jumps over the lazy dog";
+const countBrown=(story.match(/the/gi))||[] .length));
+console.log("Numbers of occurence of`the`:",countThe);
+console.log("Numbers of occurence of `brown`:", counThe);
+
+const string1="The pupils are reading  in the library";
+const string2="The child was sitting  on the table before he felt";
+console.log({index:string1.indexOf(`are`)})
+console .log(index.string2.indexOf(`sitting`));
+
+var word="Wonderful";
+word= word.toUpperCase();
+console.log(word)
+
+var word="amazing"
+word= word.toLowerCase();
+console.log(word)
+
+var word="Wonderful"
+word= word.toUndERneath();
+console.log(word)
